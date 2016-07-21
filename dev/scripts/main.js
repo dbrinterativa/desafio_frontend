9e11002953da7d95ca47f2c87f15e3795de2c6fb
Daniel Simao <simaodeveloper@gmail.com>
2016-07-21 02:45:38 -0300

    Scrollpage animation implemented

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,6 +1,16 @@
 (function(){
 	'use strict';
 
+    (function(){
+        $.fn.isOnScreen = function(p){
+            var $this = $(this),
+                $window = $(window);
+
+            return $this.offset().top <= ($window.height() - p) + $window.scrollTop();
+        };
+    })();
+
+    // Slide Questions
     var Slide = {
         time: 4000,
         run: function() {
@@ -28,6 +38,51 @@
             }, Slide.time);
         }
     });
+
+    // Animation
+
+    $(window)
+        .on('scroll', function(){
+            var wait    = 0,
+                $window = $(this);
+
+            $('[data-ani-type]:not([data-animated])').each(function(i){
+                if ($window.width() > 1024) {
+                    var $this   = $(this);
+                    var p       = $this.attr('data-ani-show') || 150;
+                    var delay   = $this.attr('data-ani-delay') || .4;
+                    var time    = $this.attr('data-ani-time') || .6;
+
+                    delay = parseFloat(delay);
+
+                    if ($this.isOnScreen(p)) {
+                        wait += delay;
+                        $this.attr('data-animated', true);
+
+                        if ($this.attr('data-ani-type') == 'zoomIn') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, scaleX: 0, scaleY: 0, y: 0, ease: Quint.easeOut});
+                        } else if ($this.attr('data-ani-type') == 'fade') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, ease: Quint.easeOut});
+                        } else if ($this.attr('data-ani-type') == 'fade-top') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, y: 60, ease: Quint.easeOut});
+                        } else if ($this.attr('data-ani-type') == 'fade-right') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, x: -60, ease: Quint.easeOut});
+                        } else if ($this.attr('data-ani-type') == 'fade-bottom') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, y: -30, ease: Quint.easeOut});
+                        } else if ($this.attr('data-ani-type') == 'fade-left') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, x: 60, ease: Quint.easeOut});
+                        } else if ($this.attr('data-ani-type') == 'zoom') {
+                            TweenLite.from($this, time, {delay: wait, opacity: 0, scale: 0.7, ease: Back.easeOut});
+                        }
+                    }
+                }
+            });
+        })
+        .on('resize', function() {
+            $(this).trigger('scroll')
+        }).trigger('resize');
+
+    // Toggle selector Sex Form
 	   
     var $selector = document.querySelector('.toggle-selector .selector');
 
