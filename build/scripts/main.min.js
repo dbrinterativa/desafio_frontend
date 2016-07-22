c423ccced3f9f6a3bb915e2d4d34845d795d0118
Daniel Simao <simaodeveloper@gmail.com>
2016-07-22 01:58:43 -0300

    Working...

--- build/scripts/main.min.js
+++ build/scripts/main.min.js
@@ -21,7 +21,7 @@
                 loaderProgress      = 100,
                 progress            = 0,
                 newProgress         = 0,
-                time                = 100;
+                time                = 300;
 
             Preloader.intervalId = setInterval(function() {
                 progress = $paceProgress.attr('data-progress-text').replace('%', '');
@@ -42,27 +42,16 @@
         $('main').css('opacity', '0');
 
         Preloader.run(function() {
-            TweenLite.to('.preloader', .4, { alpha: 0, onComplete: function() {
-                TweenLite.set('.preloader', { css: { 'display': 'none' }});
+            TweenLite.to('.preloader', .3, { delay: .2, alpha: 0, onComplete: function() {
+                // TweenLite.set('.preloader', { css: { 'display': 'none' }});
             }});
-            TweenLite.to('main', .4, { alpha: 1});
+            TweenLite.to('main', .4, { delay: .2, alpha: 1});
         });
     });
 
     $(document).on('DOMContentLoaded', function() {
-        Pace.restart();
+        Pace.start();
     });
-
-    // Background Parallax
-    window.addEventListener('scroll', function(e) {
-        // console.log(e);
-    });
-
-    document.addEventListener('scroll', function(e) {
-        console.log(e);
-    });
-
-    
 
     // Slide Questions
 
@@ -134,7 +123,15 @@
             });
         })
         .on('resize', function() {
-            $(this).trigger('scroll')
+            var $header = document.querySelector('header');
+
+            $(this).trigger('scroll');
+
+            if($(this).width() <= 959) {
+                $header.style.height = $(this).height()+'px';
+            } else {
+                $header.setAttribute('style', '');
+            }
         }).trigger('resize');
 
     // Toggle selector Sex Form
@@ -157,11 +154,95 @@
         }
     });
 
-    document.querySelector('form').addEventListener('submit', function(e) {
-        e.preventDefault();
+    var validator = new FormValidator('form-box', [{
+        name: 'form-name',
+        display: '*Favor informar seu nome.',
+        rules: 'required'
+    },{
+        name: 'form-email',
+        display: '*Favor informar seu e-mail.',
+        rules: 'required|valid_email'
+    },{
+        name: 'form-old',
+        display: 'Favor informar Idade.',
+        rules: 'required|is_natural'
+    },{
+        name: 'form-salary',
+        display: 'Favor informar pretensão salarial.',
+        rules: 'required'
+    },{
+        name: 'form-phone',
+        display: 'Favor informar seu telefone.',
+        rules: 'required|callback_check_phone'
+    },{
+        name: 'form-fb',
+        display: 'Favor informar o endereço corretamente',
+        rules: 'required|callback_check_facebook'
+    },{
+        name: 'form-tt',
+        display: 'Favor informar o endereço corretamente',
+        rules: 'required|callback_check_twitter'
+    },{
+        name: 'form-gh',
+        display: 'Favor informar o endereço corretamente',
+        rules: 'required|callback_check_github'
+    },{
+        name: 'form-message',
+        display: 'Mande uma mensagem de no minimo 10 palavras',
+        rules: 'required|min:10'
+    }], function(errors, event) {
+        event.preventDefault(0);
 
-        var a = this.querySelector('input[checked="true"]');
+        var inputs  = Array.prototype.slice.call(event.target.querySelectorAll('.input > *'));
+        
+        inputs.forEach(function(element) {
+            element.classList.remove('error');
+        });
 
+        if(errors.length > 0) {
+            errors.forEach(function(error) {
+                TweenLite.to(window, .6, { scrollTo: '#form-box' , ease: Expo.easeInOut , onComplete: function() {
+                    error.element.nextElementSibling.textContent = error.display;
+                    error.element.classList.add('error');
+                }});
+            });            
+        } else {
+            TweenLite.to('.inner-form-box', .3, { alpha: 0, onComplete:function() {
+                TweenLite.set('.message-success', { zIndex: 5 });
+                TweenLite.to('.message-success-text', .3, { alpha: 1 });
+            }});
+        }
+    });
+
+    var reFacebookPerfilUrl = /(https?:\/\/)(www)?\.?(facebook)\.([a-zA-Z0-9_]+)\.?([a-zA-Z0-9_]*)?\/?([a-zA-Z0-9_\.]*)/ig,
+        reTwitterPerfilUrl = /(https?:\/\/)(www)?\.?(twitter)\.([a-zA-Z0-9_]+)\.?([a-zA-Z0-9_]*)?\/?([a-zA-Z0-9_\.]*)/ig,
+        reGithubPerfilUrl = /(https?:\/\/)(www)?\.?(github)\.([a-zA-Z0-9_]+)\.?([a-zA-Z0-9_]*)?\/?([a-zA-Z0-9_\.]*)/ig;
+
+    validator.registerCallback('check_phone', function(value) {
+        if (value.match(/\(?\d{2}\)?\s?\d?\d{4}-?\d{4}/)) {
+            return true;
+        }
+        return false;
+    });
+
+    validator.registerCallback('check_facebook', function(value) {
+        if (value.match(reFacebookPerfilUrl)) {
+            return true;
+        }
+        return false;
+    });
+
+    validator.registerCallback('check_twitter', function(value) {
+        if (value.match(reTwitterPerfilUrl)) {
+            return true;
+        }
+        return false;
+    });
+
+    validator.registerCallback('check_github', function(value) {
+        if (value.match(reGithubPerfilUrl)) {
+            return true;
+        }
         return false;
     });
 
